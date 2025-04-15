cd6a72e0d75d786e9aa3d36f36b3148767b80cd2
Shirotha <shirotha+git@mailfence.com>
2025-04-15 13:35:34 +0200

    fix(flake): various fixes for devshell

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     crate2nix.url = "github:nix-community/crate2nix";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    devshell.url = "github:numtide/devshell";
     systems.url = "github:nix-systems/default-linux";
   };
   
@@ -13,18 +14,15 @@
     extra-trusted-public-keys = "eigenvalue.cachix.org-1:ykerQDDa55PGxU25CETy9wF6uVDpadGGXYrFNJA3TUs=";
   };
 
-  outputs = { nixpkgs, crate2nix, rust-overlay, systems, ...}: let
+  outputs = { nixpkgs, crate2nix, rust-overlay, devshell, systems, ... }: let
     inherit (nixpkgs.lib) genAttrs mapAttrs;
     systems' = import systems;
     pkgs = genAttrs systems' (system: import nixpkgs {
       inherit system;
-      overlays = [(import rust-overlay) (self: super: 
-      assert !(super ? rust-toolchain); rec {
+      overlays = [devshell.overlays.default (import rust-overlay) (self: super: 
+      assert !(super ? rust-toolchain); {
         rust-toolchain = super.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-        # NOTE: for buildRustCrate/crate2nix
-        rustc = rust-toolchain;
-        cargo = rust-toolchain;
-      }];
+      })];
       config = {};
     });
     cargoNix = genAttrs systems' (system:
@@ -34,22 +32,32 @@
       }
     );
   in {
-    checks = mapAttrs cargoNix (system: cargoNix':
+    checks = cargoNix |> mapAttrs (system: cargoNix': {
       rustnix = cargoNix'.rootCrate.build.override {
         runTests = true;
-      }
-    );
-    packages = mapAttrs cargoNix (system: cargoNix': {
-      default = cargoNix'.rootCrate.bulid;
-    );
-    devShells = mapAttrs pkgs (system: pkgs': {
-      default = with pkgs; mkShell {
-        packages = [
-          rust-toolchain
+      };
+    });
+    packages = cargoNix |> mapAttrs (system: cargoNix': let
+      pkgs' = pkgs.${system};
+    in rec {
+      default = rustnix;
+      rustnix = cargoNix'.rootCrate.build;
+      inherit (pkgs') rust-toolchain;
+      rust-toolchain-versions = pkgs'.writeScriptBin "rust-toolchain-versions" /* bash */ ''
+        ${rust-toolchain}/bin/cargo --version
+        ${rust-toolchain}/bin/rustc --version
+      '';
+    });
+    devShells = pkgs |> mapAttrs (system: pkgs': {
+      default = pkgs'.devshell.mkShell ({ ... }: {
+        imports = [
+          "${devshell}/extra/language/c.nix"
         ];
-        shellHook = /* bash */ ''
-        '';
+        commands = with pkgs'; [
+          { package = rust-toolchain; category = "rust"; }
+          { package = mold; category = "build"; }
+        ];
       });
-    };
-  }
+    });
+  };
 }
